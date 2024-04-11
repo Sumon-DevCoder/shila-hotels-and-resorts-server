36b3646fbd915c69c0331b74c19800d446972e92
SumonRahman24 <mustafizurrahmansumon24@gmail.com>
2024-04-11 13:20:34 +0600

    first commit

--- index.js
+++ index.js
@@ -1,39 +1,16 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 4000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.thpkita.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-async function run() {
-  try {
-    // await client.connect();
-    // await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
-}
-run().catch(console.dir);
-
 app.get("/", (req, res) => {
-  res.send("server is running...");
+  console.log(`server is running...`);
 });
 
 app.listen(port, () => {
-  console.log(`server is running successfully at localhost://${port}`);
+  console.log(`Server is running successfully at http://localhost:${port}`);
 });
