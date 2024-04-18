af16f4909d18e7792b279a6f06db8f11571a5ab3
SumonRahman24 <mustafizurrahmansumon24@gmail.com>
2024-04-18 12:38:16 +0600

    first commit

--- index.js
+++ index.js
@@ -1,16 +1,56 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
 require("dotenv").config();
-const port = process.env.PORT || 4000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wwcicg7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // await client.connect();
+    // await client.db("admin").command({ ping: 1 });
+
+    const userCollection = client.db("ShilaHotelDB").collection("users");
+
+    app.post("/users", async (req, res) => {
+      const userData = req.body;
+
+      const query = { email: userData.email };
+      const existsEmail = await userCollection.findOne(query);
+      if (existsEmail) {
+        return res.send({ message: "user already in exists" });
+      }
+
+      const result = await userCollection.insertOne(userData);
+      res.send(result);
+    });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
-  console.log(`server is running...`);
+  res.send("server is running...");
 });
 
 app.listen(port, () => {
-  console.log(`Server is running successfully at http://localhost:${port}`);
+  console.log(`server is running successfully at http://localhost:${port}`);
 });
